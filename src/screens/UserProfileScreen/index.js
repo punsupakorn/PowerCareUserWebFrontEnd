33311634d05047401d4d89334abcd38e7bb083d6
panitaknd <panita.22@mail.kmutt.ac.th>
2021-10-25 12:42:43 +0700

    line login3

--- src/screens/UserProfileScreen/index.js
+++ src/screens/UserProfileScreen/index.js
@@ -24,7 +24,7 @@ export default function UserProfileScreen() {
   }, []);
 
  const initLine = () => {
-    liff.init({ liffId: '1655665373-YAopzeO6' }, () => {
+    liff.init({ liffId: '1656423908-vEgA2gn7' }, () => {
       if (liff.isLoggedIn()) {
         runApp();
       } else {
