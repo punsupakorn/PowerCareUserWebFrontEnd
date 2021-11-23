67e69f5f3cbe37d9da28f0b9bfd45f5d3a9405ca
mook22 <panita.22@mail.kmutt.ac.th>
2021-11-24 00:44:51 +0700

    deploy

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
-export const apiURL = "http://localhost:8085/";
+//export const apiURL = "http://localhost:8085/";
 // export const apiURL = "https://powercareuserback.firebaseapp.com/";
-// export const apiURL = "https://powercare-user.herokuapp.com/";
+ export const apiURL = "https://powercare-user.herokuapp.com/";
 
 export const server = {
   USERPROFILE: `${apiURL}UserProfile`,
