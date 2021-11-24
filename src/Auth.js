7ff1fe5a1230e0ae7d1f5f9ec0ff2a57fcb18aa7
mook22 <panita.22@mail.kmutt.ac.th>
2021-11-25 01:06:24 +0700

    [edit] liff id

--- src/Auth.js
+++ src/Auth.js
@@ -30,7 +30,7 @@ export const AuthProvider = ({ children }) => {
 
   const initLine = async () => {
     liff.init(
-      { liffId: "1656423908-vEgA2gn7" },
+      { liffId: "1656423908-z2DErD50" },
       () => {
         if (liff.isLoggedIn({ redirectUri: "https://main.d3w2lvda55pxgd.amplifyapp.com/" })) {
           runApp();
