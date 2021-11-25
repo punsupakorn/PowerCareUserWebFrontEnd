a98c65674b5aea12ba6fc7ff2369f13b05b4fb33
Supakorn Songhong <Supakorn.pun@gmail.com>
2021-11-26 02:13:22 +0700

    up

--- src/screens/PostponeSelectScreen/index.js
+++ src/screens/PostponeSelectScreen/index.js
@@ -78,7 +78,7 @@ export default function PostponeSelectScreen() {
                   type="text"
                   name="name"
                   id="name"
-                  placeholder={olddate}
+                  placeholder={oldtime}
                   disabled
                   className="w-full px-3 py-2  border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
                 />
