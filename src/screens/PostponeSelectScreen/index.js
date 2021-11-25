4dd9f01d9ba910cf88eaad71132b38b3941df193
Supakorn Songhong <Supakorn.pun@gmail.com>
2021-11-26 03:51:15 +0700

    fix

--- src/screens/PostponeSelectScreen/index.js
+++ src/screens/PostponeSelectScreen/index.js
@@ -200,7 +200,7 @@ export default function PostponeSelectScreen() {
                       })}
                     >
                       {" "}
-                      {time.Time}{" "}
+                      {time}{" "}
                     </option>
                   ))}
                 </select>
