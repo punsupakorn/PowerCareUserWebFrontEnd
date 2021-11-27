f3f820f3476de859ad52264a9beaf0842a23b6c5
Darispb <daris.pb@mail.kmutt.ac.th>
2021-11-27 18:57:45 +0700

    loading out

--- src/screens/EditUserProfileScreen/index.js
+++ src/screens/EditUserProfileScreen/index.js
@@ -118,12 +118,8 @@ export default function EditUserProfileScreen() {
  
 
   if (loading === true) {
-    // return <div className="loading">loading...</div>;
- <div className="w-full h-full fixed block top-0 left-0 bg-white opacity-75 z-50">
-  <span className="text-white-500 opacity-75 top-1/2 my-0 mx-auto block relative w-0 h-0" style={{top: '50%'}}>
-    <i className="fas fa-circle-notch fa-spin fa-5x" />
-  </span>
-</div>
+    return <div className="loading">loading...</div>;
+
 
 
   } else {
