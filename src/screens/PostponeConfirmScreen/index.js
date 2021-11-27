67c461200a491383db0c1c26f37bebf9570722b2
Supakorn Songhong <Supakorn.pun@gmail.com>
2021-11-27 10:26:40 +0700

    edit

--- src/screens/PostponeConfirmScreen/index.js
+++ src/screens/PostponeConfirmScreen/index.js
@@ -54,8 +54,8 @@ export default function PostponeConfirmScreen() {
           setphone(res.data.Phone);
           setemail(res.data.Email);
           setdob(res.data.DateOfBirth);
-          setloading(false);
-        });
+          
+        }).then(setloading(false));
     } catch (error) {}
   };
 
