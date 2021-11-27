d00ee90eeec62aa247412dba4b1d43eda5c5964b
Supakorn Songhong <Supakorn.pun@gmail.com>
2021-11-27 10:26:02 +0700

    edit

--- src/screens/PostponeConfirmScreen/index.js
+++ src/screens/PostponeConfirmScreen/index.js
@@ -17,7 +17,7 @@ export default function PostponeConfirmScreen() {
   const [phone, setphone] = useState("");
   const [email, setemail] = useState("");
   const [dob, setdob] = useState("");
-
+  const [loading, setloading] = useState(true);
   const {
     appointmentid,
     oldtimetableid,
@@ -54,6 +54,7 @@ export default function PostponeConfirmScreen() {
           setphone(res.data.Phone);
           setemail(res.data.Email);
           setdob(res.data.DateOfBirth);
+          setloading(false);
         });
     } catch (error) {}
   };
@@ -90,220 +91,224 @@ export default function PostponeConfirmScreen() {
     } catch (error) {}
   };
 
-  return (
-    <div classname="bg-indigo-200 h-screen w-screen">
-      <div className="flex items-center min-h-screen bg-indigo-200 dark:bg-gray-900">
-        <div className="container mx-auto">
-          <div className="max-w-md mx-auto my-10 bg-white p-5 rounded-md shadow-sm">
-            <div className="text-center">
-              <h1 className="my-3 text-3xl font-semibold fontsize-18 text-gray-700 dark:text-gray-200">
-                ยืนยันการเลื่อนนัด{" "}
-              </h1>
+  if (loading === true) {
+    return <div>loading...</div>;
+  } else {
+    return (
+      <div classname="bg-indigo-200 h-screen w-screen">
+        <div className="flex items-center min-h-screen bg-indigo-200 dark:bg-gray-900">
+          <div className="container mx-auto">
+            <div className="max-w-md mx-auto my-10 bg-white p-5 rounded-md shadow-sm">
+              <div className="text-center">
+                <h1 className="my-3 text-3xl font-semibold fontsize-18 text-gray-700 dark:text-gray-200">
+                  ยืนยันการเลื่อนนัด{" "}
+                </h1>
 
-              <p className="text-gray-400 dark:text-gray-400">
-                โปรดตรวจสอบข้อมูลของท่านให้ถูกต้อง
-              </p>
-              <div
-                className="
-      flex
-      justify-between
-      items-center
-      w-full
-      py-2
-      border-b-2 border-gray-200
-    "
-              />
-            </div>
-            <div className="m-7">
-              <div className="mb-6">
-                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-400 font-bold text-20 ">
-                  ยืนยันการทำนัด :{" "}
-                </label>
-                <p className="text-base text-left text-gray-400">
-                  {" "}
-                  <b>ชื่อ - นามสกุล :</b> {username}
+                <p className="text-gray-400 dark:text-gray-400">
+                  โปรดตรวจสอบข้อมูลของท่านให้ถูกต้อง
                 </p>
                 <div
                   className="
-      flex
-      justify-between
-      items-center
-      w-50
-      py-2
-      border-b-2 border-gray-100
-    "
-                />
-                <p className="mt-2 text-base text-left text-gray-400">
-                  {" "}
-                  <b>วัน/เดือน/ปีเกิด :</b> {displayShortThaiDate(dob)}{" "}
-                </p>
-                <div
-                  className="
-      flex
-      justify-between
-      items-center
-      w-50
-      py-2
-      border-b-2 border-gray-100
-    "
-                />
-                <p className="mt-2 text-base text-left text-gray-400">
-                  {" "}
-                  <b> เพศ :</b> {sex}{" "}
-                </p>
-                <div
-                  className="
-      flex
-      justify-between
-      items-center
-      w-50
-      py-2
-      border-b-2 border-gray-100
-    "
-                />
-                <p className=" mt-2 text-base text-left text-gray-400">
-                  {" "}
-                  <b>ที่อยู่ :</b> {address}
-                </p>
-                <div
-                  className="
-      flex
-      justify-between
-      items-center
-      w-50
-      py-2
-      border-b-2 border-gray-100
-    "
-                />
-                <p className="mt-2 text-base text-left text-gray-400">
-                  {" "}
-                  <b>เบอร์ติดต่อ :</b> {phone}
-                </p>
-                <div
-                  className="
-      flex
-      justify-between
-      items-center
-      w-50
-      py-2
-      border-b-2 border-gray-100
-    "
-                />
-                <p className="mt-2 text-base text-left text-gray-400">
-                  {" "}
-                  <b>e-mail</b> : {email}{" "}
-                </p>
-                <div
-                  className="
-      flex
-      justify-between
-      items-center
-      w-50
-      py-2
-      border-b-2 border-gray-100
-    "
-                />
-                <p className="mt-2 text-base text-left text-gray-400">
-                  {" "}
-                  <b> อาการ :</b> {symptom}{" "}
-                </p>
-                <div
-                  className="
-      flex
-      justify-between
-      items-center
-      w-50
-      py-2
-      border-b-2 border-gray-100
-    "
-                />
-                <p className="mt-2 text-base text-left text-gray-400">
-                  {" "}
-                  <b> วันทำนัด :</b> {displayShortThaiDate(newdate)}{" "}
-                </p>
-                <div
-                  className="
-      flex
-      justify-between
-      items-center
-      w-50
-      py-2
-      border-b-2 border-gray-100
-    "
-                />
-                <p className="mt-2 text-base text-left text-gray-400">
-                  {" "}
-                  <b>เวลา :</b> {newtime}
-                </p>
-                <div
-                  className="
-      flex
-      justify-between
-      items-center
-      w-50
-      py-2
-      border-b-2 border-gray-100
-    "
-                />
-                <p className="mt-2 text-base text-left text-gray-400">
-                  {" "}
-                  <b>แพทย์ :</b> {doctorname}{" "}
-                </p>
-                <div
-                  className="
-      flex
-      justify-between
-      items-center
-      w-50
-      py-2
-      border-b-2 border-gray-100
-    "
+        flex
+        justify-between
+        items-center
+        w-full
+        py-2
+        border-b-2 border-gray-200
+      "
                 />
               </div>
-
-              <div className="mb-6">
-                <button
-                  className="w-full px-3 py-3  text-white bg-indigo-300 rounded-md focus:bg-indigo-200 focus:outline-none"
-                  onClick={editappointment}
-                >
-                  ยืนยันการเลื่อนนัด
-                </button>
-                <Modal
-                  open={openFirst}
-                  onClose={() => setOpenFirst(false)}
-                  center
-                >
-                  <center>
-                    <div className="w-80">
-                      <HiCheckCircle size="150px" color="#66bb6a" />
-                    </div>
-                    <p className="font-bold">เลื่อนทำนัดสำเร็จ </p>
-                  </center>
-                  <center>
-                    {/* <Link to="/menuhome"> */}
-                    <div className="mb-6">
-                      <button
-                        onClick={backToMenu}
-                        className="w-80 px-1 py-3 text-white bg-gray-300 rounded-md mt-3"
-                      >
-                        กลับสู่หน้าหลัก
-                      </button>
-                    </div>
-                    {/* </Link> */}
-                  </center>
-                </Modal>
-              </div>
-              {/* <Link to="/postponeselect">
+              <div className="m-7">
                 <div className="mb-6">
-                  <button className="w-full px-3 py-3 text-white bg-gray-300 rounded-md ">
-                    ย้อนกลับ
-                  </button>
+                  <label className="block mb-2 text-sm text-gray-600 dark:text-gray-400 font-bold text-20 ">
+                    ยืนยันการทำนัด :{" "}
+                  </label>
+                  <p className="text-base text-left text-gray-400">
+                    {" "}
+                    <b>ชื่อ - นามสกุล :</b> {username}
+                  </p>
+                  <div
+                    className="
+        flex
+        justify-between
+        items-center
+        w-50
+        py-2
+        border-b-2 border-gray-100
+      "
+                  />
+                  <p className="mt-2 text-base text-left text-gray-400">
+                    {" "}
+                    <b>วัน/เดือน/ปีเกิด :</b> {displayShortThaiDate(dob)}{" "}
+                  </p>
+                  <div
+                    className="
+        flex
+        justify-between
+        items-center
+        w-50
+        py-2
+        border-b-2 border-gray-100
+      "
+                  />
+                  <p className="mt-2 text-base text-left text-gray-400">
+                    {" "}
+                    <b> เพศ :</b> {sex}{" "}
+                  </p>
+                  <div
+                    className="
+        flex
+        justify-between
+        items-center
+        w-50
+        py-2
+        border-b-2 border-gray-100
+      "
+                  />
+                  <p className=" mt-2 text-base text-left text-gray-400">
+                    {" "}
+                    <b>ที่อยู่ :</b> {address}
+                  </p>
+                  <div
+                    className="
+        flex
+        justify-between
+        items-center
+        w-50
+        py-2
+        border-b-2 border-gray-100
+      "
+                  />
+                  <p className="mt-2 text-base text-left text-gray-400">
+                    {" "}
+                    <b>เบอร์ติดต่อ :</b> {phone}
+                  </p>
+                  <div
+                    className="
+        flex
+        justify-between
+        items-center
+        w-50
+        py-2
+        border-b-2 border-gray-100
+      "
+                  />
+                  <p className="mt-2 text-base text-left text-gray-400">
+                    {" "}
+                    <b>e-mail</b> : {email}{" "}
+                  </p>
+                  <div
+                    className="
+        flex
+        justify-between
+        items-center
+        w-50
+        py-2
+        border-b-2 border-gray-100
+      "
+                  />
+                  <p className="mt-2 text-base text-left text-gray-400">
+                    {" "}
+                    <b> อาการ :</b> {symptom}{" "}
+                  </p>
+                  <div
+                    className="
+        flex
+        justify-between
+        items-center
+        w-50
+        py-2
+        border-b-2 border-gray-100
+      "
+                  />
+                  <p className="mt-2 text-base text-left text-gray-400">
+                    {" "}
+                    <b> วันทำนัด :</b> {displayShortThaiDate(newdate)}{" "}
+                  </p>
+                  <div
+                    className="
+        flex
+        justify-between
+        items-center
+        w-50
+        py-2
+        border-b-2 border-gray-100
+      "
+                  />
+                  <p className="mt-2 text-base text-left text-gray-400">
+                    {" "}
+                    <b>เวลา :</b> {newtime}
+                  </p>
+                  <div
+                    className="
+        flex
+        justify-between
+        items-center
+        w-50
+        py-2
+        border-b-2 border-gray-100
+      "
+                  />
+                  <p className="mt-2 text-base text-left text-gray-400">
+                    {" "}
+                    <b>แพทย์ :</b> {doctorname}{" "}
+                  </p>
+                  <div
+                    className="
+        flex
+        justify-between
+        items-center
+        w-50
+        py-2
+        border-b-2 border-gray-100
+      "
+                  />
                 </div>
-              </Link> */}
+
+                <div className="mb-6">
+                  <button
+                    className="w-full px-3 py-3  text-white bg-indigo-300 rounded-md focus:bg-indigo-200 focus:outline-none"
+                    onClick={editappointment}
+                  >
+                    ยืนยันการเลื่อนนัด
+                  </button>
+                  <Modal
+                    open={openFirst}
+                    onClose={() => setOpenFirst(false)}
+                    center
+                  >
+                    <center>
+                      <div className="w-80">
+                        <HiCheckCircle size="150px" color="#66bb6a" />
+                      </div>
+                      <p className="font-bold">เลื่อนทำนัดสำเร็จ </p>
+                    </center>
+                    <center>
+                      {/* <Link to="/menuhome"> */}
+                      <div className="mb-6">
+                        <button
+                          onClick={backToMenu}
+                          className="w-80 px-1 py-3 text-white bg-gray-300 rounded-md mt-3"
+                        >
+                          กลับสู่หน้าหลัก
+                        </button>
+                      </div>
+                      {/* </Link> */}
+                    </center>
+                  </Modal>
+                </div>
+                {/* <Link to="/postponeselect">
+                  <div className="mb-6">
+                    <button className="w-full px-3 py-3 text-white bg-gray-300 rounded-md ">
+                      ย้อนกลับ
+                    </button>
+                  </div>
+                </Link> */}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
