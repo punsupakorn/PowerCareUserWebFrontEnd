b8c1f7260aae8f80d70e1e767b448ec28236d5f8
Darispb <daris.pb@mail.kmutt.ac.th>
2021-11-27 20:08:39 +0700

    loading4

--- src/screens/EditUserProfileScreen/index.js
+++ src/screens/EditUserProfileScreen/index.js
@@ -118,8 +118,8 @@ export default function EditUserProfileScreen() {
  
 
   if (loading === true) {
-    // return <div className="loading">loading...</div>;
- return <div class="fixed top-0 left-0 z-50 w-screen h-screen flex items-center justify-center" style="background: rgba(0, 0, 0, 0.3);">
+    return <div className="loading">
+ <div class="fixed top-0 left-0 z-50 w-screen h-screen flex items-center justify-center" style="background: rgba(0, 0, 0, 0.3);">
   <div class="bg-white border py-2 px-5 rounded-lg flex items-center flex-col">
     <div class="loader-dots block relative w-20 h-5 mt-2">
       <div class="absolute top-0 mt-1 w-3 h-3 rounded-full bg-green-500"></div>
@@ -132,6 +132,7 @@ export default function EditUserProfileScreen() {
     </div>
   </div>
   </div>
+  </div>;
 
 
   } else {
