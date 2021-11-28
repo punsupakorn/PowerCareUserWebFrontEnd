67a0183101c8e4dac96207da256695cc880f4380
Supakorn Songhong <Supakorn.pun@gmail.com>
2021-11-28 09:06:38 +0700

    return fix

--- src/screens/AppointmentScreen/index.js
+++ src/screens/AppointmentScreen/index.js
@@ -15,32 +15,12 @@ export default function AppointmentScreen() {
   const [doctorname, setdoctorname] = useState("");
   const [timetableid, settimetableid] = useState("");
   const [time, settime] = useState("");
-  const [loading, setloading] = useState(true);
-
-  const checkuserappointment = () => {
-    try {
-      let accessToken = localStorage.getItem("AccessToken");
-      axios.get(`${server.APPOINTMENT}/check/${accessToken}`).then((res) => {
-        if (res.data == true) {
-          getDate();
-        } else {
-          window.alert("ขออภัย คุณมีการทำนัดเอาไว้อยู่แล้ว");
-          history.replace("/menuhome");
-        }
-      });
-    } catch (error) {}
-  };
 
   const getDate = () => {
     try {
-      axios
-        .get(server.APPOINTMENT)
-        .then((res) => {
-          setDateSlot(res.data);
-        })
-        .then((res) => {
-          setloading(false);
-        });
+      axios.get(server.APPOINTMENT).then((res) => {
+        setDateSlot(res.data);
+      });
     } catch (error) {}
   };
 
@@ -70,8 +50,7 @@ export default function AppointmentScreen() {
   };
 
   useEffect(async () => {
-    // getDate();
-    checkuserappointment();
+    getDate();
   }, []);
 
   const displayThaiDate = (date) => {
@@ -146,172 +125,168 @@ export default function AppointmentScreen() {
     }
   };
 
-  if (loading === true) {
-    return <div>loading...</div>;
-  } else {
-    return (
-      <div classname="bg-indigo-200 h-screen w-screen">
-        <div className="flex items-center min-h-screen bg-indigo-200 dark:bg-gray-900">
-          <div className="container mx-auto">
-            <div className="max-w-md mx-auto my-10 bg-white p-5 rounded-md shadow-sm">
-              <div className="text-center">
-                <h1 className="my-3 text-3xl font-semibold fontsize-18 text-gray-700 dark:text-gray-200">
-                  ทำนัด
-                </h1>
+  return (
+    <div classname="bg-indigo-200 h-screen w-screen">
+      <div className="flex items-center min-h-screen bg-indigo-200 dark:bg-gray-900">
+        <div className="container mx-auto">
+          <div className="max-w-md mx-auto my-10 bg-white p-5 rounded-md shadow-sm">
+            <div className="text-center">
+              <h1 className="my-3 text-3xl font-semibold fontsize-18 text-gray-700 dark:text-gray-200">
+                ทำนัด
+              </h1>
 
-                <p className="text-gray-400 dark:text-gray-400">
-                  โปรดกรอกข้อมูลของท่านให้ถูกต้อง
-                </p>
-                <div
-                  className="
-          flex
-          justify-between
-          items-center
-          w-full
-          py-2
-          border-b-2 border-gray-200
-        "
-                />
-              </div>
-              <div className="m-7">
-                <form>
-                  <div className="mb-6">
-                    <label className="block mb-2 text-sm text-gray-600 dark:text-gray-400">
-                      อาการ <h className="text-red-600"> * </h>
-                    </label>
-                    <textarea
-                      rows={5}
-                      name="message"
-                      id="message"
-                      placeholder="โปรดกรอกอาการของท่าน"
-                      className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
-                      required
-                      defaultValue={""}
-                      onChange={handleSymptom}
-                    />
-                  </div>
-                  <div className="mb-6">
-                    <label className="block mb-2 text-sm text-gray-600 dark:text-gray-400">
-                      เลือกวันที่ <h className="text-red-600"> * </h>
-                    </label>
-                    <select
-                      onChange={handleDate}
-                      // value={DateSlot}
-                      // id="position"
-                      // name="Position"
-                      className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500 "
-                    >
-                      <option disabled selected value>
+              <p className="text-gray-400 dark:text-gray-400">
+                โปรดกรอกข้อมูลของท่านให้ถูกต้อง
+              </p>
+              <div
+                className="
+        flex
+        justify-between
+        items-center
+        w-full
+        py-2
+        border-b-2 border-gray-200
+      "
+              />
+            </div>
+            <div className="m-7">
+              <form>
+                <div className="mb-6">
+                  <label className="block mb-2 text-sm text-gray-600 dark:text-gray-400">
+                    อาการ {" "}  <h className="text-red-600"> * </h>
+                  </label>
+                  <textarea
+                    rows={5}
+                    name="message"
+                    id="message"
+                    placeholder="โปรดกรอกอาการของท่าน"
+                    className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                    required
+                    defaultValue={""}
+                    onChange={handleSymptom}
+                  />
+                </div>
+                <div className="mb-6">
+                  <label className="block mb-2 text-sm text-gray-600 dark:text-gray-400">
+                    เลือกวันที่  <h className="text-red-600"> * </h>
+                  </label>
+                  <select
+                    onChange={handleDate}
+                    // value={DateSlot}
+                    // id="position"
+                    // name="Position"
+                    className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500 "
+                  >
+                    <option disabled selected value>
+                      {" "}
+                      กรุณาเลือกวันที่
+                    </option>
+                    {DateSlot.map((slot) => (
+                      <option
+                        className="option"
+                        value={JSON.stringify({
+                          slot: `${slot}`,
+                        })}
+                        // onChange={getDoctorName}
+                      >
                         {" "}
-                        กรุณาเลือกวันที่
+                        {displayThaiDate(slot)}{" "}
                       </option>
-                      {DateSlot.map((slot) => (
-                        <option
-                          className="option"
-                          value={JSON.stringify({
-                            slot: `${slot}`,
-                          })}
-                          // onChange={getDoctorName}
-                        >
-                          {" "}
-                          {displayThaiDate(slot)}{" "}
-                        </option>
-                      ))}
-                    </select>
-                  </div>
-                  <div className="mb-6">
-                    <label className="block mb-2 text-sm text-gray-600 dark:text-gray-400">
-                      เลือกแพทย์ <h className="text-red-600"> * </h>
-                    </label>
-                    <select
-                      // id="position"
-                      // name="Position"
-                      className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500 "
-                      onChange={handleDoctorName}
-                    >
-                      <option disabled selected value>
+                    ))}
+                  </select>
+                </div>
+                <div className="mb-6">
+                  <label className="block mb-2 text-sm text-gray-600 dark:text-gray-400">
+                    เลือกแพทย์ {" "}  <h className="text-red-600"> * </h> 
+                  </label>
+                  <select
+                    // id="position"
+                    // name="Position"
+                    className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500 "
+                    onChange={handleDoctorName}
+                  >
+                    <option disabled selected value>
+                      {" "}
+                      กรุณาเลือกแพทย์
+                    </option>
+                    {DoctorName.map((doctor) => (
+                      <option
+                        className="option"
+                        value={JSON.stringify({
+                          timetableid: `${doctor.TimeTableID}`,
+                          doctorid: `${doctor.DocumentID}`,
+                          name: `${doctor.Name}`,
+                        })}
+                      >
                         {" "}
-                        กรุณาเลือกแพทย์
+                        {doctor.Name}{" "}
                       </option>
-                      {DoctorName.map((doctor) => (
-                        <option
-                          className="option"
-                          value={JSON.stringify({
-                            timetableid: `${doctor.TimeTableID}`,
-                            doctorid: `${doctor.DocumentID}`,
-                            name: `${doctor.Name}`,
-                          })}
-                        >
-                          {" "}
-                          {doctor.Name}{" "}
-                        </option>
-                      ))}
-                    </select>
-                  </div>
-                  <div className="mb-6">
-                    <label className="block mb-2 text-sm text-gray-600 dark:text-gray-400">
-                      เลือกเวลา <h className="text-red-600"> * </h>
-                    </label>
-                    <select
-                      // id="position"
-                      // name="Position"
-                      className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500 "
-                      onChange={handleTime}
-                    >
-                      <option disabled selected value>
+                    ))}
+                  </select>
+                </div>
+                <div className="mb-6">
+                  <label className="block mb-2 text-sm text-gray-600 dark:text-gray-400">
+                    เลือกเวลา <h className="text-red-600"> * </h>
+                  </label>
+                  <select
+                    // id="position"
+                    // name="Position"
+                    className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500 "
+                    onChange={handleTime}
+                  >
+                    <option disabled selected value>
+                      {" "}
+                      กรุณาเลือกเวลา
+                    </option>
+                    {Time.map((slot) => (
+                      <option
+                        className="option"
+                        value={JSON.stringify({
+                          time: `${slot}`,
+                        })}
+                      >
                         {" "}
-                        กรุณาเลือกเวลา
+                        {slot}{" "}
                       </option>
-                      {Time.map((slot) => (
-                        <option
-                          className="option"
-                          value={JSON.stringify({
-                            time: `${slot}`,
-                          })}
-                        >
-                          {" "}
-                          {slot}{" "}
-                        </option>
-                      ))}
-                    </select>
-                  </div>
+                    ))}
+                  </select>
+                </div>
 
-                  {/* <Link
-                    to={{
-                      pathname: `/appointmentconfirm`,
-                      state: {
-                        symptom: symptom,
-                        date: date,
-                        doctorname: doctorname,
-                        doctorid: doctorid,
-                        timetableid: timetableid,
-                        time: time,
-                      },
-                    }}
-                  > */}
+                {/* <Link
+                  to={{
+                    pathname: `/appointmentconfirm`,
+                    state: {
+                      symptom: symptom,
+                      date: date,
+                      doctorname: doctorname,
+                      doctorid: doctorid,
+                      timetableid: timetableid,
+                      time: time,
+                    },
+                  }}
+                > */}
+                <div className="mb-6">
+                  <button
+                    type="button"
+                    className="w-full px-3 py-3 text-white bg-indigo-300 rounded-md focus:bg-indigo-200 focus:outline-none"
+                    onClick={handleNext}
+                  >
+                    ถัดไป
+                  </button>
+                </div>
+                {/* </Link> */}
+                <Link to="/menuhome">
                   <div className="mb-6">
-                    <button
-                      type="button"
-                      className="w-full px-3 py-3 text-white bg-indigo-300 rounded-md focus:bg-indigo-200 focus:outline-none"
-                      onClick={handleNext}
-                    >
-                      ถัดไป
+                    <button className="w-full px-3 py-3 text-white bg-gray-300 rounded-md ">
+                      ย้อนกลับ
                     </button>
                   </div>
-                  {/* </Link> */}
-                  <Link to="/menuhome">
-                    <div className="mb-6">
-                      <button className="w-full px-3 py-3 text-white bg-gray-300 rounded-md ">
-                        ย้อนกลับ
-                      </button>
-                    </div>
-                  </Link>
-                </form>
-              </div>
+                </Link>
+              </form>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
